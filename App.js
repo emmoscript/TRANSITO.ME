fae7421246628b08eea6d5ff8e02c74c99e24e47
Emil0309 <71798519+Emil0309@users.noreply.github.com>
2024-04-11 23:14:00 -0400

    first commit

--- App.js
+++ App.js
@@ -1,18 +1,42 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import CreateAccountScreen from './src/screens/CreateAccountScreen';
-import HomeScreen from './src/screens/HomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import SettingsScreen from "./screens/SettingsScreen";
+import CourseListScreen from "./screens/CourseList";
+import ProfileScreen from "./screens/Profile";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { AboutStack } from "./AppStack";
 
-const AppNavigator = createStackNavigator(
-  {
-     MenúPrincipal: HomeScreen,
-     IniciarSesión: LoginScreen,
-     CrearCuenta: CreateAccountScreen
-  },
-  {
-     initialRouteName: 'CrearCuenta'
-  }
-);
+const Tab = createBottomTabNavigator();
 
-export default createAppContainer(AppNavigator);
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          //   tabBarShowLabel: false,
+          tabBarLabelPosition: "below-icon",
+          tabBarActiveTintColor: "purple",
+        }}
+      >
+        <Tab.Screen name="Course List" component={CourseListScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: "My Profile",
+            tabBarIcon: () => <Ionicons name={"person"} size={20} />,
+            tabBarBadge: 3,
+          }}
+        />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen
+          name="About Stack"
+          component={AboutStack}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
